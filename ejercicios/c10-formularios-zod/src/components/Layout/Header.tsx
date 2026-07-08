005da5f8e82ce57671b611b2e0564bd9b153b13a
Juan Ignacio Martinez <juanygnaciomartinez17@gmail.com>
2026-07-07 21:23:05 -0300

    AI-C10: alta de libro con formulario controlado y validacion Zod

---
+++ ejercicios/c10-formularios-zod/src/components/Layout/Header.tsx
@@ -0,0 +1,19 @@
+import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+function Header() {
+    return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+        <Navbar.Brand as={Link} to="/">📖 Librería</Navbar.Brand>
+        <Nav className="ms-auto">
+            <Nav.Link as={Link} to="/">Inicio</Nav.Link>
+            <Nav.Link as={Link} to="/catalogo">Catálogo</Nav.Link>
+            <Nav.Link as={Link} to="/libros/nuevo">Nuevo libro</Nav.Link>
+        </Nav>
+        </Container>
+    </Navbar>
+    );
+}
+
+export default Header;
